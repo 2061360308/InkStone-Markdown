9918e692f798216c338dc2a252e2f998ff297a39
2061360308 <2061360308@qq.com>
2025-02-08 18:55:02 +0800

    feat: 拦截默认全局右键菜单事件

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount } from 'vue'
+
+onMounted(() => {
+  const handleContextMenu = (event: MouseEvent) => {
+    event.preventDefault()
+  }
+
+  // 添加全局的 contextmenu 事件监听器
+  window.addEventListener('contextmenu', handleContextMenu)
+
+  // 在组件卸载前移除事件监听器
+  onBeforeUnmount(() => {
+    window.removeEventListener('contextmenu', handleContextMenu)
+  })
+})
+</script>
 
 <template>
   <router-view></router-view>
